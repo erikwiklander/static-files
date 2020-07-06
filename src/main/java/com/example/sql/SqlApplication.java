a6cd39d640d2de2bc3afb801dfe7234e2a019e8b
Erik Wiklander <erik_wiklander@aisreview.com>
2020-07-06 08:44:12 -0500

    Creted String Beans

--- src/main/java/com/example/sql/SqlApplication.java
+++ src/main/java/com/example/sql/SqlApplication.java
@@ -1,15 +1,11 @@
 package com.example.sql;
 
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.Resource;
-import org.springframework.util.StreamUtils;
-
-import java.nio.charset.StandardCharsets;
 
 
 @Slf4j
@@ -21,19 +17,12 @@ public class SqlApplication {
     }
 
     @Bean
-    public ApplicationRunner start(SqlProperties properties) {
+    public ApplicationRunner start(@Qualifier("file1") String s1, @Qualifier("file2") String s2) {
         return args -> {
-            log.info(getContent(properties.getFile1()));
-            log.info(getContent(properties.getFile2()));
+            log.info(s1);
+            log.info(s2);
         };
 
     }
-
-
-    @SneakyThrows
-    private static String getContent(Resource r) {
-        return StreamUtils.copyToString(r.getInputStream(), StandardCharsets.UTF_8);
-    }
-
 
 }
--- src/main/java/com/example/sql/SqlProperties.java
+++ src/main/java/com/example/sql/SqlProperties.java
@@ -2,9 +2,14 @@ package com.example.sql;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.SneakyThrows;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
+
+import java.nio.charset.StandardCharsets;
 
 @Getter
 @Setter
@@ -13,4 +18,19 @@ import org.springframework.stereotype.Component;
 public class SqlProperties {
     private Resource file1;
     private Resource file2;
+
+    @Bean
+    public String file1() {
+        return getContent(file1);
+    }
+    @Bean
+    public String file2() {
+        return getContent(file2);
+    }
+
+    @SneakyThrows
+    private static String getContent(Resource r) {
+        return StreamUtils.copyToString(r.getInputStream(), StandardCharsets.UTF_8);
+    }
+
 }
